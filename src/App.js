a0be4725f67585bfafc41be739a36d5c54530761
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-03-10 19:50:57 -0600

    cambios 3
    se usa los hooks useEfect y useState para poder atraer la informacion del pokemon .
    *se hace la conexión  con la api de pokemon
    * se creo la función de Random para lo pokemon
    *se muestra las imágenes de los pokemon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[pokemon,setPokemon]= useState({});
+  const fetchPokemon=(id)=>{
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response)=>response.json())
+    .then((data)=>setPokemon(data));
+  };
+
+  const getRandomInt=(min= 1,max=600)=>{
+    return Math.floor(Math.random()* (max-min)+min);
+  };
+
+  useEffect(()=>{
+    console.log({pokemon});
+  },[pokemon]);
   return (
     <div className="App">
       <header className="App-header">
         <div className='flex-container'>
-        <img src={logo} className="poke-image" alt="logo" />
-        <img src={logo} className="poke-image" alt="logo" />
+        <img src={pokemon?.sprites?.front_default} className="poke-image" alt="logo" />
+        <img src={pokemon?.sprites?.back_default} className="poke-image" alt="logo" />
         </div>
+        <h3>Nombre del pokemon:</h3>
         <p>
-         Nombre
+         {pokemon.name ?? "Pokemon no seleccionado"}
         </p>
         <div className='flex-continer '>
           <button className='button'>Antes</button>
-          <button className='button'>Random</button>
+          <button className='button' onClick={()=>fetchPokemon(getRandomInt())}>Random</button>
           <button className='button'>Siguiente</button>
         </div>
       </header>
